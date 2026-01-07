fcbf40bb69b04b877081174a72841e6955160972
Nikolay Serduchenko <nik.serdu201584@gmail.com>
2026-01-08 01:10:21 +0300

    chore: add CAMERA_PACKAGE client

--- lib/grpc/registry/grpc.registry.ts
+++ lib/grpc/registry/grpc.registry.ts
@@ -4,6 +4,11 @@ export const GRPC_CLIENTS = {
 	AUTH_PACKAGE: {
 		package: 'auth.v1',
 		protoPath: PROTO_PATHS.AUTH,
-		env: 'AUTH_GRPC_URL'
+		env: 'AUTH_GRPC_URL',
+	},
+	CAMERA_PACKAGE: {
+		package: 'camera.v1',
+		protoPath: PROTO_PATHS.CAMERA,
+		env: 'CAMERA_GRPC_URL',
 	},
 } as const
