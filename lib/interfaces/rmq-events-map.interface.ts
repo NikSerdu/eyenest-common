dc79d9c75a66dc6885e60eb7c2e8f8b2dd3812db
Nikolay Serduchenko <nik.serdu201584@gmail.com>
2026-03-20 15:36:20 +0300

    fix: update RmqEventMap interface to require cameraId for recording events and change roomId for join/leave events

--- lib/interfaces/rmq-events-map.interface.ts
+++ lib/interfaces/rmq-events-map.interface.ts
@@ -1,11 +1,11 @@
 import { Events } from '../enums'
 
 export interface RmqEventMap {
-	[Events.CAMERA_START_RECORDING]: { cameraId?: string }
-	[Events.CAMERA_STOP_RECORDING]: { cameraId?: string }
+	[Events.CAMERA_START_RECORDING]: { cameraId: string }
+	[Events.CAMERA_STOP_RECORDING]: { cameraId: string }
 
-	[Events.CAMERA_JOIN]: { cameraId: string }
-	[Events.CAMERA_LEAVE]: { cameraId: string }
+	[Events.CAMERA_JOIN]: { roomId: string }
+	[Events.CAMERA_LEAVE]: { roomId: string }
 
 	[Events.MOTION_DETECTED]: { cameraId: string; timestamp: number }
 	[Events.MOTION_ON]: { cameraId: string }
